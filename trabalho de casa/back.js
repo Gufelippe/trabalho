8773877be7b981acff31578ea5b6e9bc0c21800a
Gufelippe <91915124+Gufelippe@users.noreply.github.com>
2022-08-16 16:59:56 -0300

    Add files via upload

--- trabalho de casa/back.js
+++ trabalho de casa/back.js
@@ -1,3 +1,33 @@
-function Alert_text(){
+function Alert_text() {
     alert("teste");
 }
+
+function acertou(){
+    acertou(1);
+}
+
+
+function trocarJanela(alvo) {
+
+    let pagAlvo = window.getComputedStyle(document.querySelector(alvo));
+
+    if (pagAlvo.className === "amostra") {
+        document.getElementById(alvo).className.add('amostra');
+    }
+
+    switch (alvo) {
+
+        case 1:
+            document.getElementById("1").className.add('a-amostra');
+            document.getElementById("1").className.remove('amostra');
+            break;
+
+    }
+
+
+}
+
+function errou() {
+
+}
+
